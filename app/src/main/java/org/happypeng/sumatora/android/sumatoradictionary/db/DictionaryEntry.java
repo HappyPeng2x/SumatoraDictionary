778c4fcc4ca0ad20a0eb10cdd26714b8e4c06717
Nicolas Centa <nicolas.centa@happypeng.org>
2019-01-05 16:50:38 +0900

    First usable version using room.
    Moved database import to AssetSQLiteOpenHelper for performance reasons.

--- app/src/main/java/org/happypeng/sumatora/android/sumatoradictionary/db/DictionaryEntry.java
+++ app/src/main/java/org/happypeng/sumatora/android/sumatoradictionary/db/DictionaryEntry.java
@@ -1,3 +1,19 @@
+/* Sumatora Dictionary
+        Copyright (C) 2019 Nicolas Centa
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
 package org.happypeng.sumatora.android.sumatoradictionary.db;
 
 import androidx.recyclerview.widget.DiffUtil;
@@ -37,10 +53,10 @@ public class DictionaryEntry {
 
         DictionaryEntry dictionaryEntry = (DictionaryEntry) obj;
 
-        return dictionaryEntry.readings == dictionaryEntry.readings &&
-                dictionaryEntry.writings == dictionaryEntry.writings &&
-                dictionaryEntry.lang == dictionaryEntry.lang &&
-                dictionaryEntry.gloss == dictionaryEntry.gloss &&
-                dictionaryEntry.bookmark == dictionaryEntry.bookmark;
+        return readings.equals(dictionaryEntry.readings) &&
+                writings.equals(dictionaryEntry.writings) &&
+                lang.equals(dictionaryEntry.lang) &&
+                gloss.equals(dictionaryEntry.gloss) &&
+                bookmark.equals(dictionaryEntry.bookmark);
     }
 }
